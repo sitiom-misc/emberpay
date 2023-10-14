7162c1df7e4839c19e0a46337aea8d61fd16a32e
Ryan Caezar Itang <sitiom@proton.me>
2023-10-14 18:24:52 +0800

    feat: initial firebase setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,27 @@ import { Material3ThemeProvider } from "@/lib/Material3ThemeProvider";
 import { Stack } from "expo-router";
 import { Appbar } from "react-native-paper";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+import {
+  AuthProvider,
+  FirebaseAppProvider,
+  FirestoreProvider,
+  StorageProvider,
+  useFirebaseApp,
+  useInitAuth,
+} from "reactfire";
+import { connectAuthEmulator, initializeAuth } from "firebase/auth";
+import { reactNativeLocalPersistence } from "firebase/auth/react-native";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAd_anZDfjCFs5OVhWVgYDAbi2Hy1AJ2Y",
+  authDomain: "emberpay-iac.firebaseapp.com",
+  projectId: "emberpay-iac",
+  storageBucket: "emberpay-iac.appspot.com",
+  messagingSenderId: "692675967448",
+  appId: "1:692675967448:web:00d56d1233bafd64481aad",
+};
 
 function Navbar({ navigation, back }: NativeStackHeaderProps) {
   return (
@@ -12,14 +33,47 @@ function Navbar({ navigation, back }: NativeStackHeaderProps) {
   );
 }
 
+function FirebaseProviders({ children }: { children: React.ReactNode }) {
+  const app = useFirebaseApp();
+  const firestore = getFirestore(app);
+  const storage = getStorage(app);
+  const { status: authInitStatus, data: auth } = useInitAuth(async (app) =>
+    initializeAuth(app, {
+      persistence: reactNativeLocalPersistence,
+    })
+  );
+
+  if (authInitStatus === "loading") {
+    return null;
+  }
+
+  if (__DEV__) {
+    connectFirestoreEmulator(firestore, "localhost", 8080);
+    connectAuthEmulator(auth, "http://localhost:9099");
+    connectStorageEmulator(storage, "localhost", 9199);
+  }
+
+  return (
+    <AuthProvider sdk={auth}>
+      <FirestoreProvider sdk={firestore}>
+        <StorageProvider sdk={storage}>{children}</StorageProvider>
+      </FirestoreProvider>
+    </AuthProvider>
+  );
+}
+
 export default function RootLayout() {
   return (
-    <Material3ThemeProvider sourceColor="#fbc02d">
-      <Stack
-        screenOptions={{
-          header: (props) => <Navbar {...props} />,
-        }}
-      />
-    </Material3ThemeProvider>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <FirebaseProviders>
+        <Material3ThemeProvider sourceColor="#fbc02d">
+          <Stack
+            screenOptions={{
+              header: (props) => <Navbar {...props} />,
+            }}
+          />
+        </Material3ThemeProvider>
+      </FirebaseProviders>
+    </FirebaseAppProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,30 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
+import { useAuth, useSigninCheck, useUser } from "reactfire";
 
 export default function IndexScreen() {
+  const { status: signInStatus, data: signInCheckResult } = useSigninCheck();
+  const auth = useAuth();
+
+  useEffect(() => {
+    // Test authentication
+    signInWithEmailAndPassword(auth, "john@gmail.com", "john123");
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      {signInStatus === "loading" ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text>Signed in: {signInCheckResult.signedIn ? "Yes" : "No"}</Text>
+          {signInCheckResult.signedIn && (
+            <Text>User: {signInCheckResult.user.displayName}</Text>
+          )}
+        </>
+      )}
     </View>
   );
 }
